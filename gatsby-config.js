a3525d6d6fec5574b2c28bdd875c4fecf616e662
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-08-05 14:58:22 +0200

    fix gtag + add cname plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,20 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     {
-      resolve: "gatsby-plugin-google-gtag",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ["G-D296ZNCS72"],
+        trackingIds: [
+          "G-D296ZNCS72"
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     {
